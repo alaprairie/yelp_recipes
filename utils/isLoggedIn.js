1d8187c733de2b80bb3445fbdd73060014f8d893
ani <ariane.laprairie@coker.edu>
2020-11-19 19:02:30 +0000

    Add flash error message

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -2,6 +2,7 @@ const isLoggedIn = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		return next();
 	} else {
+		req.flash("error", "Hey! Please log in to do that.");
 		res.redirect("/login");
 	}
 };
