6f8cb806b6f9ecd57a5ecb40df7046510a776ad3
ani <ariane.laprairie@coker.edu>
2020-11-27 19:26:40 +0000

    Refactor vote function

--- public/js/recipes_show.js
+++ public/js/recipes_show.js
@@ -5,18 +5,23 @@ const upvoteBtn = document.getElementById("upvote_btn");
 const downvoteBtn = document.getElementById("downvote_btn");
 
 // ==========================
-// ADD EVENT LISTENERS
+// HELPER FUNCTIONS
 // ==========================
-upvoteBtn.addEventListener("click", async function() {
+const sendVote = async (voteType) => {
 	// Build fetch options
 	const options = {
 		method: "POST",
 		headers: {
 			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({vote: "up"})
+		}
 	}
-	
+	if (voteType === "up") {
+		options.body = JSON.stringify({vote: "up"});
+	} else if (voteType === "down") {
+		options.body = JSON.stringify({vote: "down"});
+	} else {
+		throw "voteType must be 'up' or 'down'"
+	}
 	// Send fetch request
 	await fetch("/recipes/vote", options)
 	.then(data => {
@@ -28,4 +33,15 @@ upvoteBtn.addEventListener("click", async function() {
 	.catch(err => {
 		console.log(err)
 	})
+}
+
+// ==========================
+// ADD EVENT LISTENERS
+// ==========================
+upvoteBtn.addEventListener("click", async function() {
+	sendVote("up")
+})
+
+downvoteBtn.addEventListener("click", async function() {
+	sendVote("down")
 })
